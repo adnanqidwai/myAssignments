d2262343e07a30c938488e5013f2b350de323f70
adnan <adgta25@gmail.com>
2022-01-18 14:57:30 +0530

    updating roots problem

--- assignment 3/quadRoot.c
+++ assignment 3/quadRoot.c
@@ -2,36 +2,35 @@
 #include <math.h>
 
 int main(){
-    double a[3]; double root1,root2;
-    for (int i = 0; i < 3; i++)
-    {
-        a[i]=0;
-    }
-    for (int i = 0; i < 3; i++)
-    {
-        scanf("%lf",&a[i]);
-    }
-    double D = (a[1]*a[1])-4*a[0]*a[2];
-    
-    if (D>0)
-    {
-        root1= ((-1*a[1]) + D)/(2*a[0]);
-        root2= ((-1*a[1]) - D)/(2*a[0]);
-        printf("%lf %lf",root1,root2);
-    }
-    
-    else if (D<0)
-    {
-        D= D/(2*a[0]);
-        a[0]= -1*a[0]/(2*a[0]);
-        printf("%lf+ i%lf ",a[0],D);
-        printf("%lf- i%lf",a[0],D);
-    }
-    else
-    {
-        root1= -1*a[1]/(2*a[0]);
-        printf("%lf",root1);
-    }
-    
-    
+    int t; scanf("%d",&t);
+    while(t--){
+        long int a[3]; double b[3], root1,root2;
+        for (int i = 0; i < 3; i++)
+        {
+            a[i]=0;
+            b[i]=0.0;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            scanf("%ld",&a[i]);
+        }
+        
+        long int D = (a[1]*a[1])-4*a[0]*a[2];
+        if (D>=0)
+        {
+            double d= sqrt(D);
+            root1= ((-1.0*a[1]) + d)/(2*a[0]); 
+            root2= ((-1.0*a[1]) - d)/(2*a[0]);
+            printf("%.8lf 0.00000000\n%.8lf 0.00000000\n",root1,root2);
+        }
+        
+        else
+        {
+            double d= sqrt((-1)*D);
+            double c = d/(2.0*a[0]);
+            float b = -1.0*a[1]/(2.0*a[0]);
+            printf("%.8lf %.8lf\n",b,c);
+            printf("%.8lf %.8lf\n",b,(-1)*c);
+        }
+    }        
 }
