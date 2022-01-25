4c585d56dfed5033ce1beb13c5c7bd6f634232a8
adnan <adgta25@gmail.com>
2022-01-26 01:21:48 +0530

    ass4

---
+++ assignment 4/1mildice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int arr[6]={0},x=0;
+    for (int i = 0; i < 1e6; i++)
+    {
+        arr[rand()%6]++;
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        printf("%d ==> %d\n",i+1,arr[i]);
+    }
+    
+}
