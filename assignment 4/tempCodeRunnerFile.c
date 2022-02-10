b045e22c8f1da2ae093741448914b35e08288900
adnan <adgta25@gmail.com>
2022-02-11 00:38:05 +0530

    as4

--- assignment 4/tempCodeRunnerFile.c
+++ assignment 4/tempCodeRunnerFile.c
@@ -1,1 +1,13 @@
-int key[n];
+  while ((ch=fgetc(ptr))!='<')
+    {
+        a[ch]++;
+        printf("%c",ch);
+    }
+    for (int i = 1; i < 128; i++)
+    {
+        if (a[i]!=0)
+        {
+            printf("%c occurs %d times\n",i,a[i]);
+        }
+        
+    }
--- assignment 4/test.c
+++ assignment 4/test.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int strinput(char str[])
-{
-    int ch, i = 0;
-    while ((ch = getchar()) != '\n')
-        (str[i++]) = ch;
-    (str[i]) = '\0';
-    return i;
-}
+int main(){
 
-int main()
-{
-    printf("%d", 2*(__INT_MAX__+1));
+    printf("%d",rand());
 }
