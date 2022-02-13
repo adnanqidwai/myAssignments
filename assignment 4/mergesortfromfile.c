c2ce09357501901f510e5701ede165d690bfa031
adnan <adgta25@gmail.com>
2022-02-13 12:59:57 +0530

    as4

--- assignment 4/mergesortfromfile.c
+++ assignment 4/mergesortfromfile.c
@@ -11,36 +11,37 @@ int compare(const void *a, const void *b)
 
 int main(){
     int n1=0,n2=0;
-    char temp1[100];
-    char temp2[100];
-    char *all[1000];
     char temp[100];
+    char all[1000][100];
     FILE *ptr1=fopen("sorted1.txt","r");
     FILE *ptr2=fopen("sorted2.txt","r");
     FILE *ptr3=fopen("finalsorted.txt","w+");
     
-    for (int i = 0; fscanf(ptr1,"%s",temp1)==1;i++)
+    for (int i = 0; fscanf(ptr1,"%s",temp)==1;i++)
     {
-        all[i]=temp1;
-        printf("%s\n",all[i]);
+        strcpy(all[i],temp);
         n1++;
     }
-    for (int i = n1; fscanf(ptr2,"%s",temp2)==1;i++)
+    n2= n1;
+    for (int i = n1; fscanf(ptr2,"%s",temp)==1;i++)
     {
-        all[i]=temp2;
-        printf("%s\n",all[i]);
+        strcpy(all[i],temp);
         n2++;
     }
-    for (int i = 0; i < n1+n2; i++)
-    {
-        printf("%s\n",all[i]);
-    }
+    printf("\ncheck your file\n\n");
     
-    qsort(all, n1+n2, sizeof(const char*),compare);
-        
-    for (int i = 0; i < n1+n2; i++)
+    for(int i=0; i<n2; i++){
+        for(int j=0; j<n2-1-i; j++){
+            if(strcmp(all[j], all[j+1]) > 0){
+                strcpy(temp, all[j]);
+                strcpy(all[j], all[j+1]);
+                strcpy(all[j+1], temp); 
+            }
+        }
+    }            
+    for (int i = 0; i < n2; i++)
     {
-        //printf("%s\n",all[i]);
+        fprintf(ptr3,"%s\n",all[i]);
     }
     
     fclose(ptr1);
